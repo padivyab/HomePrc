09d7ec71b023952c49236a0be74b25dbac3200fe
Divya <divyapatel.dp908@gmail.com>
2022-07-13 21:17:49 -0400

    data

---
+++ HomeworkPro/src/data/Sample.java
@@ -0,0 +1,8 @@
+package data;
+
+public class Sample {
+    public static void main(String args[])
+    {
+        System.out.println("divya");
+    }
+}
---
+++ HomeworkPro/src/data/simple.java
@@ -0,0 +1,9 @@
+package data;
+
+public class simple {
+    public static void maim(String args[])
+    {
+        System.out.println("divya");
+
+    }
+}
